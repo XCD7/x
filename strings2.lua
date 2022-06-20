2f3185296d9cd2c6dd820f96e8bec5ad57370d75
XCD7 <107736049+XCD7@users.noreply.github.com>
2022-06-19 19:11:41 -0700

    Update strings2.lua

--- strings2.lua
+++ strings2.lua
@@ -20,9 +20,8 @@
 		'',
 }
 	-- Randomizes Text --
-	wait(5)
 	while true do
 		wait(1)
-		print(hrases[math.random(1, #phrases)])
+		print(phrases[math.random(1, #phrases)])
 		script.Parent.Text = phrases[math.random(1, #phrases)]
 	end
